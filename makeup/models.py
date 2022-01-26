387d4cd0f38d608756b249ed7d41208e8c60c2bb
Saba Fadhl <sabafadl98@gmail.com>
2022-01-26 23:11:37 +0300

    models creation

--- makeup/models.py
+++ makeup/models.py
@@ -1,3 +1,23 @@
+from tokenize import Name
 from django.db import models
 
-# Create your models here.
+class Brand(models.Model):
+    '''
+    this model test brand
+    '''
+    
+    Name=models.CharField(max_length=50)
+    Orgin=models.CharField(max_length=50)
+
+class Products(models.Model):
+    '''
+    this model test brand
+    '''
+    Name=models.CharField(max_length=50)
+    Kind=models.CharField(max_length=50)
+    Descreption=models.CharField(max_length=50)
+    Expir_date=models.DateField()
+    Price=models.IntegerField()
+    brand=models.ForeignKey(Brand,on_delete=models.CASCADE,null=True)
+    
+    
