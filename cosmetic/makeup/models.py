6b71e8c795c6d5f966250197ada2fbb99e9f5332
SabaFadhl <sabafadl98@gmail.com>
2022-02-01 13:44:11 +0300

    fix header menu

---
+++ cosmetic/makeup/models.py
@@ -0,0 +1,36 @@
+from tokenize import Name
+from django.db import models
+from django.core.validators import MinValueValidator
+class Brand(models.Model):
+    '''
+    this model test brand
+    '''
+    
+    name=models.CharField(max_length=50)
+    orgin=models.CharField(max_length=50)
+    
+    def __str__(self) :
+        return self.name
+
+    def get_absolute_url(self):
+        return f"/brands/{self.name}/"
+    
+    
+    
+class Products(models.Model):
+    '''
+    this model test brand
+    '''
+    name=models.CharField(max_length=50)
+    kind=models.CharField(max_length=50)
+    descreption=models.TextField(null=True)
+    expir_date=models.DateField(null=True)
+    price=models.PositiveIntegerField(default=10, validators=[MinValueValidator(5)])
+    brand=models.ForeignKey(Brand,on_delete=models.CASCADE,null=True)
+    
+    def __str__(self) :
+        return self.name
+
+    # def get_absolute_url(self):
+    #     # return f"/products/{self.name}/"
+    #     pass
---
+++ cosmetic/makeup/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from . import views
+from makeup.views import ProductsList,BrandsList,ProductDetailView,SellerProducts,productUpdateView,productCreateView,productDeleteView
+
+urlpatterns = [
+    path('about/',views.about,name="about"),
+    # path('products/<int:id>',views.post_details,name="products-details"),
+    path('contact/',views.contact,name="contact"),
+    path('details/<pk>',ProductDetailView.as_view(),name="product-detail"),
+    path('brands/',BrandsList.as_view(),name="brands"),
+    path('products/',ProductsList.as_view(),name="products"),
+    path('seller/products/',SellerProducts.as_view(),name="products-seller"),
+    path('seller/products/add', productCreateView.as_view(), name='add-products'),
+    path('seller/products/edit/<int:pk>',productUpdateView.as_view(),name="edit-product"),
+    path('seller/products/delete/<int:pk>',productDeleteView.as_view(),name="delete-product"),
+    
+    
+]
